53314b104aa35c1ac12b4f3f1385677d469b0e1e
kaur6970 <kaur6970@sheridancollege.ca>
2019-06-20 17:11:37 -0400

    midterm

--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -15,25 +15,28 @@ import java.util.Scanner;
  */
 public class ArithmeticBase 
 {
- public double x,y;
+    //applying the encapsulation by making data members private
+    private double x,y;
     double calculate(double x, double y) 
         {
         Scanner sc =new Scanner(System.in);
-        System.out.println("enter String");
-        String s= sc.next();
-        switch (s.toUpperCase()) 
+        System.out.println("enter a number from 1 to 4");
+        int s= sc.nextInt();
+        if(s == 1) 
         {
-            case "PLUS":
-                return x + y;
-            case "MINUS":
-                return x - y;
-            case "TIMES":
-                return x * y;
-            case "DIVIDE":
+                return x + y;}
+        else if(s == 2) {
+            return x - y;
+        }else if(s==3) {
+           return x * y;
+        }else if(s == 4) {
                 return x / y;
-            default:
-                throw new AssertionError("Unknown operations " + this);
-        }
+        }else {
+            System.out.println("enter a number from 1 to 4");
+            s = sc.nextInt();
+            
     }
+        return 0;
    
+        }
 }
